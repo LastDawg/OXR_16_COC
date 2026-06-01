4f82569484a5b838006da536d7beb290280b1750
LastDawg <127403892+LastDawg@users.noreply.github.com>
2026-06-01 16:30:05 +0300

    Отрицательные значения бустеров

--- src/xrGame/ui/UIBoosterInfo.cpp
+++ src/xrGame/ui/UIBoosterInfo.cpp
@@ -8,6 +8,9 @@
 #include "UIXmlInit.h"
 #include "UIHelper.h"
 
+u32 const red_clr = color_argb(255, 210, 50, 50);
+u32 const green_clr = color_argb(255, 170, 170, 170);
+
 CUIBoosterInfo::CUIBoosterInfo() : CUIWindow(CUIBoosterInfo::GetDebugType()) {}
 
 CUIBoosterInfo::~CUIBoosterInfo()
@@ -219,6 +222,7 @@ UIBoosterInfoItem::UIBoosterInfoItem() : CUIWindow(UIBoosterInfoItem::GetDebugTy
 {
     m_magnitude = 1.0f;
     m_show_sign = false;
+    m_sign_inverse = false;
 
     m_unit_str._set("");
     m_texture_minus._set("");
@@ -234,6 +238,7 @@ void UIBoosterInfoItem::Init(CUIXml& xml, LPCSTR section)
     m_value = UIHelper::CreateStatic(xml, "value", this);
     m_magnitude = xml.ReadAttribFlt("value", 0, "magnitude", 1.0f);
     m_show_sign = (xml.ReadAttribInt("value", 0, "show_sign", 1) == 1);
+    m_sign_inverse = (xml.ReadAttribInt("value", 0, "sign_inverse", 0) == 1);
 
     LPCSTR unit_str = xml.ReadAttrib("value", 0, "unit_str", "");
     m_unit_str._set(StringTable().translate(unit_str));
@@ -268,7 +273,9 @@ void UIBoosterInfoItem::SetValue(float value)
     m_value->SetText(str);
 
     bool positive = (value >= 0.0f);
-    m_value->SetTextColor(color_rgba(170, 170, 170, 255));
+    positive = (m_sign_inverse) ? !positive : positive;
+    u32 color = (positive) ? green_clr : red_clr;
+    m_value->SetTextColor(color);
 
     if (m_texture_minus.size())
     {
